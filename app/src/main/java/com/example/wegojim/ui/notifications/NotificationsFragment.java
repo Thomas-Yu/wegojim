4a782bdc620714638a2ad7ca028b2989515f3581
Thomas Yu <tennisthomasyu@gmail.com>
2022-08-23 01:46:30 -0700

    Update workouts page

--- app/src/main/java/com/example/wegojim/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/wegojim/ui/notifications/NotificationsFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.wegojim.R;
 import com.example.wegojim.databinding.FragmentNotificationsBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
@@ -23,6 +29,21 @@ public class NotificationsFragment extends Fragment {
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        List<String> list = new ArrayList<String>();
+        list.add("Day 1 Upper");
+        list.add("Day 2 Lower");
+        list.add("Day 3 Upper");
+        list.add("Rest Day");
+        list.add("Day 4 Lower");
+        list.add("Day 5 Upper");
+        list.add("Rest Day");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this.getContext(), R.layout.textview, list);
+        ListView exerciseList = (ListView) root.findViewById(R.id.workoutList);
+        exerciseList.setAdapter(itemsAdapter);
+
+
 
         return root;
     }
